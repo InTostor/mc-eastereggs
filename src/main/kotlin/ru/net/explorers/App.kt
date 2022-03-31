d4126429d393e355ed7fbc23be0e9d45dd593e11
InTostor <gena05irk@yandex.ru>
2022-03-31 14:09:47 +0800

    second try of api

--- src/main/kotlin/ru/net/explorers/App.kt
+++ src/main/kotlin/ru/net/explorers/App.kt
@@ -8,13 +8,20 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.block.Block;
+import org.bukkit.Material;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.inventory.EquipmentSlot;
+
+import java.lang.Enum.*;
 
 
 
 public class App : JavaPlugin(),Listener{
+    val console=Bukkit.getConsoleSender()
 
 override public fun onEnable(){
-    Bukkit.getConsoleSender().sendMessage("§6[Easter counter] Plugin loaded!");
+    console.sendMessage("§6[Easter counter] Plugin loaded!");
     Bukkit.getServer().getPluginManager().registerEvents(this, this);
     saveDefaultConfig();
     reloadConfig();
@@ -25,17 +32,29 @@ public fun onPlayerJoin(e: PlayerJoinEvent) {
     val player = e.getPlayer();
 
     player.playSound(player.getLocation(), Sound.BLOCK_AMETHYST_BLOCK_BREAK, 1f, 1f);
-    Bukkit.getConsoleSender().sendMessage("§6[Easter counter] Event")
+    console.sendMessage("§6[Easter counter] Event")
 
     }
 
 @EventHandler
-public  fun onPlayerInteract(e: PlayerInteractEvent){
-    val player = e.getPlayer();
-    player.playSound(player.getLocation(), Sound.BLOCK_AMETHYST_BLOCK_BREAK, 1f, 1f);
+public  fun onPlayerInteract(event: PlayerInteractEvent){    
+    val player = event.getPlayer();
+    val block = event.getBlock()
+    val action = event.getAction()
+
+    if action.isRightClick(){
+    
+    if (block?.getType() == Material.BEDROCK){
+        player.playSound(player.getLocation(), Sound.AMBIENT_CAVE, 2f, 0.75f);
+        console.sendMessage("§6[Easter counter] someone clicked on bedrock");
+        console.sendMessage(player.displayName())
+        player.sendMessage("You found a secret!");
+    }
+
+
 }
 
 
-
+}
 
 }
